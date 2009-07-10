3735be7822be2e6e864852757e659a2d8f0a8fe7
Ilya Konovalov <aragaer@gmail.com>
2009-07-11 00:16:38 +0400

    Initial commit

---
+++ defaults/preferences/astr.js
@@ -0,0 +1,4 @@
+pref("toolkit.defaultChromeURI", "chrome://astr/content/astr.xul");
+pref("general.useragent.locale", "en_US");    		
+pref("browser.preferences.instantApply", true);
+pref("general.skins.selectedSkin", "classic/1.0");
